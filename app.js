154621d896bd3b84e7fb9dd9684374d25d875624
geofpie <64627837+geofpie@users.noreply.github.com>
2024-07-14 21:14:13 +0800

    Update app.js

--- app.js
+++ app.js
@@ -100,12 +100,12 @@ const verifyToken = (req, res, next) => {
     const token = req.cookies.token;
 
     if (!token) {
-        return res.status(401).json({ error: 'Unauthorized' });
+        return res.status(401).json({ redirect: '/401' });
     }
 
     jwt.verify(token, JWT_SECRET, (err, decoded) => {
         if (err) {
-            return res.status(401).json({ error: 'Unauthorized' });
+            return res.status(401).json({ redirect: '/401' });
         }
 
         req.user = decoded; // Attach the decoded token to the request object
