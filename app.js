b7db98de4c1f5b7c76feaff8786b37dd00589eef
geofpie <64627837+geofpie@users.noreply.github.com>
2024-07-15 22:07:43 +0800

    Update app.js

--- app.js
+++ app.js
@@ -542,7 +542,7 @@ app.get('/api/follow/:username', verifyToken, (req, res) => {
             INSERT INTO follows (follower_id, followed_id, status)
             VALUES (?, ?, 'requested')
         `;
-        db.query(insertFollowQuery, [followerId, profileUser.id], (err, result) => {
+        db.query(insertFollowQuery, [followerId, profileUser.user_id], (err, result) => {
             if (err) {
                 console.error('Error inserting follow request:', err);
                 return res.status(500).send('Error inserting follow request');
