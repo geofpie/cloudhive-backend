bc460d61852c1dd23c5f10511dc03f83c66371f8
geofpie <64627837+geofpie@users.noreply.github.com>
2024-07-21 13:09:54 +0800

    Update app.js

--- app.js
+++ app.js
@@ -97,7 +97,7 @@ app.post('/api/register', (req, res) => {
 
                 // Invoke Lambda function to subscribe user to SNS topic
                 const params = {
-                    FunctionName: 'cloudhiveSubscribeUser', // Lambda function ARN or name
+                    FunctionName: 'arn:aws:lambda:us-east-1:576047115698:function:cloudhiveSubscribeUser', // Lambda function ARN or name
                     InvocationType: 'Event', // Asynchronous invocation
                     Payload: JSON.stringify({ email }) // Pass email to the Lambda function
                 };
